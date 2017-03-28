e2f5da8fb8d3ce7b703ad651eecb6bc849ea229d
tao31 <670465194@qq.com>
2017-03-28 14:46:04 +0800

    初始化

---
+++ bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/ouzhongyuan/Downloads/robotium/class4/todolist/bin/AndroidManifest.xml : \
+/Users/ouzhongyuan/Downloads/robotium/class4/todolist/AndroidManifest.xml \
---
+++ bin/todolist-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+/Users/ouzhongyuan/Downloads/robotium/class4/todolist/bin/todolist-debug-unaligned.apk : \
+/Users/ouzhongyuan/Downloads/robotium/class4/todolist/bin/todolist.ap_ \
+/Users/ouzhongyuan/Downloads/robotium/class4/todolist/bin/classes.dex \
+/Users/ouzhongyuan/Downloads/robotium/class4/todolist/libs/android-support-v4.jar \
